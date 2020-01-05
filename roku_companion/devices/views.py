3305c6d730c92a95277883b6ca7347abf0bfc5a2
noah <thenoahsalas@gmail.com>
2020-01-05 09:54:32 -0500

    changed query params to return list if not command

--- roku_companion/devices/views.py
+++ roku_companion/devices/views.py
@@ -4,10 +4,6 @@ from django.db.models import Q
 from .serializer import ClientDeviceSerializer,DevicesSerializer,CommandSerializer
 from rest_framework.generics import RetrieveAPIView, ListAPIView, ListCreateAPIView
 
-from django_filters.rest_framework import DjangoFilterBackend
-from .filter import DeviceModelFilter
-
-
 class ClientDeviceView(RetrieveAPIView):
     serializer_class = ClientDeviceSerializer
     model = serializer_class.Meta.model
@@ -21,19 +17,16 @@ class DevicesListView(ListAPIView):
 class DeviceCommandsList(ListCreateAPIView):
 
     serializer_class = CommandSerializer
-    # model = DeviceCommands
     model = serializer_class.Meta.model
-
-    # print(DeviceCommands.objects.get(command='select'))
-
-    # lookup_url_kwarg =
 
     def get_queryset(self):
         query = self.request.GET
         if query:
             device_model = query.get('device_model')
             command = query.get('command')
-            queryset = DevicesModel.objects.get(device_model=device_model).commands.filter(command=command)
+            queryset = DevicesModel.objects.get(device_model=device_model).commands.all()
+            if query.get('command'):
+                queryset = queryset.filter(command=command)
             return queryset
 
 
