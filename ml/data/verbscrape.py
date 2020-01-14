dd01f552d293eeffc1e5200d26296e12dd2cb470
noah <thenoahsalas@gmail.com>
2020-01-14 09:09:13 -0500

    Made multifunctional data proccessor. Grouped methods used.

--- ml/data/verbscrape.py
+++ ml/data/verbscrape.py
@@ -4,58 +4,114 @@ import json
 import pandas as pd
 import os
 
-def GetDef(word):
+
+# return [defintions]
+def WordClean(word):
+    data = word.strip("][")
+    data = word.replace('"', "")
+    data = word.replace("'", "")
+    data = word.replace(",", "")
+    data = data.replace(";", "")
+    data = data.replace("\t", "")
+    data = data.replace("\n", "")
+    word = data.replace(" ", "")
+
+    return word
+
+
+def Get_Def(word):
+    print("Looking for data on {}".format(word))
     ses = Session()
-    req = ses.get("https://www.thesaurus.com/browse/{search_word}?s=t".format(search_word = word))
+    req = ses.get("https://www.thesaurus.com/browse/{search_word}?s=t".format(search_word=word))
     text = req.text
     soup = BeautifulSoup(text, 'html.parser')
     som = str(soup.find_all("script")[22]).strip("<script> window.INITIAL_STATE = ").strip(";</")
     search_data = json.loads(som)['searchData']['tunaApiData']['posTabs']
+    print("search data: ", search_data)
+    definitions = []
+    for phrase in search_data:
+        try:
+            preproc = phrase['definition']
+            if ";" in preproc or "," in preproc:
+                preproc = preproc.replace(";", ",").split(",")
+            definitions = preproc
+        except:
+            continue
+    if not definitions:
+        print("Failed to find data on {}".format(word))
+    print("definitions extracted", definitions)
+    return definitions
 
-    defintions = []
-    for ting in search_data:
-        preproc = ting['definition']
-        if ";" in preproc or "," in preproc:
-            preproc = preproc.replace(";", ",").split(",")
-        defintions = preproc
-    if defintions != []:
-        if type(defintions) == list:
-            defintions.append(word)
-            print("Succesfully downloaded synonyms for {}".format(word))
-            return defintions
-        else:
-            print("Succesfully downloaded synonyms for {}".format(word))
-            return [defintions,word]
 
+def ProccessData(file):
+    # definitions = []
+    word = []
+    if "txt" in file:
+        print("Attempting to process {}:".format(file))
+        os.chdir("/home/noah/Desktop/Funstuff/ml/data/wordlist/new/")
+        with open(file, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                try:
+                    words_list = line.split(" ")
+                    word = WordClean(words_list.pop())
+                    definitions = Get_Def(word)
 
-def MakeCollection(words, name):
-    # words = '["synonyms":{}]'.format(words)
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection")
-    if name in os.listdir(os.getcwd()):
-        pd.DataFrame({"phrases":[words]}).to_csv(name, mode='a', header=False)
-        print("Succefully appended new values to {name}".format(name=name))
+                    if definitions:
+                        if type(definitions) == list:
+                            definitions.append(word)
+                            print("Succesfully downloaded synonyms for {}".format(word))
+                            MakeData(definitions)
+                        else:
+                            print("Succesfully downloaded synonyms for {}".format(word))
+                            definitions = [definitions, word]
+                            MakeData(definitions)
+                except:
+                    print("Failed to get definition.")
+
+    elif "csv" in file:
+        print("Attempting to process {}:".format(file))
+        os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw/")
+        df = pd.read_csv(file)
+        df = df['phrases']
+        for line in df:
+            try:
+                words_list = line.split(" ")
+                for word in words_list:
+                    word = WordClean(word)
+                    definitions = Get_Def(word)
+                    if definitions:
+                        if type(definitions) == list:
+                            definitions.append(word)
+                            print("Succesfully downloaded synonyms for {}".format(word))
+                            MakeData(definitions)
+                        else:
+                            print("Succesfully downloaded synonyms for {}".format(word))
+                            definitions = [definitions, word]
+                            MakeData(definitions)
+            except:
+                print("Failed to get definition.")
     else:
-        pd.DataFrame({"phrases":[words]}).to_csv(name, mode='w', header=True)
-        print("Succefully appended new values to {name}".format(name=name))
+        print("Wrong file type")
 
 
-def CleanData(file):
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/wordlist/new")
-    with open(file, "r") as file:
-        print("Formatting data in file: {file}".format(file=file))
-        data = file.read()
-        data = data.replace(","," ")
-        data = data.replace(";"," ")
-        data = data.replace("\t"," ")
-        data = data.replace("\n"," ")
-        words = data.split(" ")
-        print("Completed formatting.")
-        return words
+
+
+def CleanCSVData():
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw")
+    list_dir = os.listdir(os.getcwd())
+    for filename in list_dir:
+        if ".csv" in filename:
+            df = pd.read_csv(os.getcwd() + "/" + filename)
+            phrases = df['phrases']
+            phrases = phrases.drop_duplicates()
+            phrases.to_csv("/home/noah/Desktop/Funstuff/ml/data/collection/clean/clean" + filename, mode="w",
+                           header=True)
 
 
 
 def GetName():
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/")
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw")
     listdir = os.listdir(os.getcwd())
     flist = []
     for file in listdir:
@@ -63,86 +119,50 @@ def GetName():
     if listdir == []:
         return "1.csv"
     else:
-        last_name = str(max(flist))+".csv"
-        df = pd.read_csv(os.getcwd()+"//"+last_name)
+        last_name = str(max(flist)) + ".csv"
+        df = pd.read_csv(os.getcwd() + "//" + last_name)
         if len(df['phrases']) > 50000:
-            new_name = str(max(flist) + 1)+".csv"
+            new_name = str(max(flist) + 1) + ".csv"
             return new_name
         else:
             return last_name
 
+
 def GetRecentName():
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection")
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw")
     listdir = os.listdir(os.getcwd())
     flist = []
     for file in listdir:
         flist.append(int(file.rstrip(".csv")))
     if listdir == []:
         return "1.csv"
-    return str(max(flist))+".csv"
+    return str(max(flist)) + ".csv"
 
 
-
-
-## pull words from verbsync
-# if its a phrase dont
-#else continue
+def MakeData(words):
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw")
+    name = GetName()
+    try:
+        if name in os.listdir(os.getcwd()):
+            pd.DataFrame({"phrases": [words]}).to_csv(name, mode='a', header=False)
+            print("Succefully stored new values to {name}".format(name=name))
+        else:
+            pd.DataFrame({"phrases": [words]}).to_csv(name, mode='w', header=True)
+            print("Succefully stored new values to {name}".format(name=name))
+    except:
+        print("Failed to store data on {word}".format(word=words))
 
 def MoreWords():
-    import copy
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection")
-    current = GetRecentName()
-    next_name = GetName()
-    name = copy.deepcopy(next_name)
+    print("Starting MoreWords!")
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/wordlist/new/")
+    list_dir = os.listdir(os.getcwd())
+    for file in list_dir:
+        ProccessData(file)
 
-    for file in os.listdir(os.getcwd()):
-        df = pd.read_csv("/home/noah/Desktop/Funstuff/ml/data/collection/" + current)
+    os.chdir("/home/noah/Desktop/Funstuff/ml/data/collection/raw/")
+    list_dir = os.listdir(os.getcwd())
+    for file in list_dir:
+        ProccessData(file)
+    CleanCSVData()
 
-        # phrases = df[1]
-        # for syn in phrases:
-        #     this = syn.split(",")#or maybe json load it
-        #     for ting in this:
-        #         ting = ting.split(" ")
-        #         if len(ting) == 1:
-        #             wordslist = Get_Def(ting)
-        #             make_collection(wordslist, name)
-        #         name = next_name
-
-# MoreWords()
-
-
-
-
-
-## when comes time to clean if word is repeated and has a row where its alone remove the one that is alone
-## else leave it
-#
-
-
-def Words():
-    os.chdir("/home/noah/Desktop/Funstuff/ml/data/wordlist/new")
-    listdir = os.listdir(os.getcwd())
-    total = 0
-    success_data = 0
-    for file in listdir:
-        if ".txt" in file:
-            words_list = CleanData(file)
-            total = len(words_list)
-            print("Currently finding data using: {file}".format(file=GetRecentName()))
-            if words_list != None:
-                for word in words_list:
-                    try:
-                        defs = GetDef(word)
-                        MakeCollection(defs, GetName())
-                        success_data +=1
-                    except:
-                        print("Failed to get data on {word}".format(word=word))
-                        continue
-            z = (100/success_data+total)
-            print("{file}\n".format(file=file))
-            print("Percentage: ", str(success_data*total)+"%")
-
-        z = (100/success_data+total)
-        print("Overall percentage: ", str(success_data*total)+"%")
-
-Words()
+MoreWords()
